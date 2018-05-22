525a7aa882e80f42f4ab3e2395470f58e4317ed6
david muchiri <njugidav@gmail.com>
2018-05-22 14:25:13 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './scss/styles.scss';
+
+// ReactDOM.render(<IndecisonApp />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpackConfig = {
+	entry: './src/app.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'dist')
+	}
+};
+module.exports = webpackConfig;
